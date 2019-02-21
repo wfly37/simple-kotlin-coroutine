7ecb9965ccb13929a45e4009960dc52291ddf38d
Bill Fly <w.fly@dcccd.edu>
2019-02-21 10:13:17 -0600

    Upgrade to latest Kotlin and Gradle versions. Increase JVM size.

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,15 +1,19 @@
-import kotlinx.coroutines.experimental.*
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 // runBlocking starts a top-level coroutine that blocks until its inner coroutine is active
-fun main(args: Array<String>) = runBlocking<Unit> {
+fun main() = runBlocking<Unit> {
     // create a lot of coroutines and list their jobs
-    val jobs = List(100000) {
-        launch(CommonPool) {
-            delay(1000L)
+    val jobs = List(100_000) {
+        launch {
+            delay(1_000L)
             print(".")
         }
     }
 
     // wait for all jobs to complete
     jobs.forEach { it.join() }
+    println()
 }
